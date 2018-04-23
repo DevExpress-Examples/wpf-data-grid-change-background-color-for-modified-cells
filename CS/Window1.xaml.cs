f47191289d08d78c8afa08cbfb20fa940f59a7bd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+using DevExpress.Data;
+
+namespace HighlightModifiedCells {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        const string IsEditedPrefix = "IsEdited_";
+        List<TestData> list;
+        Dictionary<CellInfo, bool> editedCells = new Dictionary<CellInfo, bool>();
+
+        public Window1() {
+            InitializeComponent();
+            list = new List<TestData>();
+            for(int i = 0; i < 20; i++) {
+                list.Add(new TestData() { Number1 = i, Number2 = i * 10, Text1 = "row " + i, Text2 = "ROW " + i  });
+            }
+
+            int columnCount = grid.Columns.Count;
+            grid.Columns.BeginUpdate();
+            for(int i = 0; i < columnCount; i++) {
+                grid.Columns[i].CellStyle = CreateStyle("CellStyle", grid.Columns[i]); 
+
+                GridColumn isEditedColumn = new GridColumn() {
+                    FieldName = IsEditedPrefix + grid.Columns[i].FieldName, 
+                    Visible = false, 
+                    UnboundType = UnboundColumnType.Boolean,
+                };
+                grid.Columns.Add(isEditedColumn);
+            }
+            grid.Columns.EndUpdate();
+            
+            grid.DataSource = list;
+        }
+        Style CreateStyle(string baseStyleResourceKey, GridColumn baseColumn) {
+            Style style = new Style() { BasedOn = (Style)FindResource(baseStyleResourceKey), TargetType = typeof(CellContentPresenter) };
+            DataTrigger trigger = new DataTrigger() { Binding = new Binding(RowData.DataContextProperty.Name + "." + IsEditedPrefix + baseColumn.FieldName), Value = true };
+            trigger.Setters.Add(new Setter() { Property = Border.BackgroundProperty, Value = Brushes.Red });
+            style.Triggers.Add(trigger);
+            return style;
+        }
+        private void grid_CustomUnboundColumnData(object sender, GridColumnDataEventArgs e) {
+            if(e.Column.FieldName.StartsWith(IsEditedPrefix)) {
+                CellInfo key = new CellInfo(e.ListSourceRowIndex, e.Column.FieldName);
+                if(e.IsGetData) {
+                    e.Value = GetIsEdited(key);
+                }
+                if(e.IsSetData) {
+                    SetIsEdited(key, (bool)e.Value);
+                }
+            }
+        }
+        bool GetIsEdited(CellInfo key) {
+            bool isSelected;
+            if(editedCells.TryGetValue(key, out isSelected))
+                return isSelected;
+            return false;
+        }
+        void SetIsEdited(CellInfo key, bool value) {
+            if(value)
+                editedCells[key] = value;
+            else
+                editedCells.Remove(key);
+        }
+
+        private void view_CellValueChanged(object sender, CellValueEventArgs e) {
+            if(!e.Column.FieldName.StartsWith(IsEditedPrefix))
+                grid.SetCellValue(e.RowHandle, IsEditedPrefix + e.Column.FieldName, true);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            List<CellInfo> tempList = new List<CellInfo>(editedCells.Keys);
+            foreach(CellInfo info in tempList) {
+                grid.SetCellValue(grid.GetRowHandleByListIndex(info.ListIndex), info.FieldName, false);
+            }
+        }
+
+    }
+    public class TestData {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+    }
+    public class CellInfo {
+        public int ListIndex { get; private set; }
+        public string FieldName { get; private set; }
+        public CellInfo(int listIndex, string fieldName) {
+            ListIndex = listIndex;
+            FieldName = fieldName;
+        }
+        public override bool Equals(object obj) {
+            CellInfo other = obj as CellInfo;
+            return other != null && other.FieldName == FieldName && other.ListIndex == ListIndex;
+        }
+        public override int GetHashCode() {
+            return ListIndex ^ FieldName.GetHashCode();
+        }
+    }
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,163 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Data
+
+Namespace HighlightModifiedCells
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Private Const IsEditedPrefix As String = "IsEdited_"
+		Private list As List(Of TestData)
+		Private editedCells As New Dictionary(Of CellInfo, Boolean)()
+
+		Public Sub New()
+			InitializeComponent()
+			list = New List(Of TestData)()
+			For i As Integer = 0 To 19
+				list.Add(New TestData() With {.Number1 = i, .Number2 = i * 10, .Text1 = "row " & i, .Text2 = "ROW " & i})
+			Next i
+
+			Dim columnCount As Integer = grid.Columns.Count
+			grid.Columns.BeginUpdate()
+			For i As Integer = 0 To columnCount - 1
+				grid.Columns(i).CellStyle = CreateStyle("CellStyle", grid.Columns(i))
+
+				Dim isEditedColumn As New GridColumn() With {.FieldName = IsEditedPrefix & grid.Columns(i).FieldName, .Visible = False, .UnboundType = UnboundColumnType.Boolean}
+				grid.Columns.Add(isEditedColumn)
+			Next i
+			grid.Columns.EndUpdate()
+
+			grid.DataSource = list
+		End Sub
+		Private Function CreateStyle(ByVal baseStyleResourceKey As String, ByVal baseColumn As GridColumn) As Style
+			Dim style As New Style() With {.BasedOn = CType(FindResource(baseStyleResourceKey), Style), .TargetType = GetType(CellContentPresenter)}
+			Dim trigger As New DataTrigger() With {.Binding = New Binding(RowData.DataContextProperty.Name & "." & IsEditedPrefix & baseColumn.FieldName), .Value = True}
+			trigger.Setters.Add(New Setter() With {.Property = Border.BackgroundProperty, .Value = Brushes.Red})
+			style.Triggers.Add(trigger)
+			Return style
+		End Function
+		Private Sub grid_CustomUnboundColumnData(ByVal sender As Object, ByVal e As GridColumnDataEventArgs)
+			If e.Column.FieldName.StartsWith(IsEditedPrefix) Then
+				Dim key As New CellInfo(e.ListSourceRowIndex, e.Column.FieldName)
+				If e.IsGetData Then
+					e.Value = GetIsEdited(key)
+				End If
+				If e.IsSetData Then
+					SetIsEdited(key, CBool(e.Value))
+				End If
+			End If
+		End Sub
+		Private Function GetIsEdited(ByVal key As CellInfo) As Boolean
+			Dim isSelected As Boolean
+			If editedCells.TryGetValue(key, isSelected) Then
+				Return isSelected
+			End If
+			Return False
+		End Function
+		Private Sub SetIsEdited(ByVal key As CellInfo, ByVal value As Boolean)
+			If value Then
+				editedCells(key) = value
+			Else
+				editedCells.Remove(key)
+			End If
+		End Sub
+
+		Private Sub view_CellValueChanged(ByVal sender As Object, ByVal e As CellValueEventArgs)
+			If (Not e.Column.FieldName.StartsWith(IsEditedPrefix)) Then
+				grid.SetCellValue(e.RowHandle, IsEditedPrefix & e.Column.FieldName, True)
+			End If
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim tempList As New List(Of CellInfo)(editedCells.Keys)
+			For Each info As CellInfo In tempList
+				grid.SetCellValue(grid.GetRowHandleByListIndex(info.ListIndex), info.FieldName, False)
+			Next info
+		End Sub
+
+	End Class
+	Public Class TestData
+		Private privateNumber1 As Integer
+		Public Property Number1() As Integer
+			Get
+				Return privateNumber1
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber1 = value
+			End Set
+		End Property
+		Private privateNumber2 As Integer
+		Public Property Number2() As Integer
+			Get
+				Return privateNumber2
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber2 = value
+			End Set
+		End Property
+		Private privateText1 As String
+		Public Property Text1() As String
+			Get
+				Return privateText1
+			End Get
+			Set(ByVal value As String)
+				privateText1 = value
+			End Set
+		End Property
+		Private privateText2 As String
+		Public Property Text2() As String
+			Get
+				Return privateText2
+			End Get
+			Set(ByVal value As String)
+				privateText2 = value
+			End Set
+		End Property
+	End Class
+	Public Class CellInfo
+		Private privateListIndex As Integer
+		Public Property ListIndex() As Integer
+			Get
+				Return privateListIndex
+			End Get
+			Private Set(ByVal value As Integer)
+				privateListIndex = value
+			End Set
+		End Property
+		Private privateFieldName As String
+		Public Property FieldName() As String
+			Get
+				Return privateFieldName
+			End Get
+			Private Set(ByVal value As String)
+				privateFieldName = value
+			End Set
+		End Property
+		Public Sub New(ByVal listIndex As Integer, ByVal fieldName As String)
+            privateListIndex = listIndex
+            privateFieldName = fieldName
+		End Sub
+		Public Overrides Overloads Function Equals(ByVal obj As Object) As Boolean
+			Dim other As CellInfo = TryCast(obj, CellInfo)
+			Return other IsNot Nothing AndAlso other.FieldName = FieldName AndAlso other.ListIndex = ListIndex
+		End Function
+		Public Overrides Function GetHashCode() As Integer
+			Return ListIndex Xor FieldName.GetHashCode()
+		End Function
+	End Class
+End Namespace

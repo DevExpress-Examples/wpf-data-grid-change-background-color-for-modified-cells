6433140770b80d260b08ddcbdee020decfc87688
DevExpressExampleBot <fakeEmail>
2023-02-21 15:06:03 +0400

    Source auto update [skip ci]

--- VB/HighlightChangedCellBehavior.vb
+++ VB/HighlightChangedCellBehavior.vb
@@ -1,4 +1,3 @@
-﻿Imports DevExpress.Data
 Imports DevExpress.Mvvm.UI.Interactivity
 Imports DevExpress.Xpf.Grid
 Imports System
@@ -8,84 +7,94 @@ Imports System.Windows
 Imports System.Windows.Media
 
 Namespace HighlightModifiedCells
-	Public Class ChangedCellsHighlightBehavior
-		Inherits Behavior(Of GridControl)
 
-		Private Const UnboundColumnPrefix As String = "IsEdited_"
-		Private modifiedCells As New Dictionary(Of Tuple(Of Object, String), Boolean)()
-		Private originalValues As New Dictionary(Of Tuple(Of Object, String), Object)()
-		Public Shared ReadOnly HighlightBrushProperty As DependencyProperty = DependencyProperty.Register("HighlightBrush", GetType(Brush), GetType(ChangedCellsHighlightBehavior))
-		Public Property HighlightBrush() As Brush
-			Get
-				Return CType(GetValue(HighlightBrushProperty), Brush)
-			End Get
-			Set(ByVal value As Brush)
-				SetValue(HighlightBrushProperty, value)
-			End Set
-		End Property
-		Protected ReadOnly Property View() As TableView
-			Get
-				Return CType(AssociatedObject.View, TableView)
-			End Get
-		End Property
-		Protected Overrides Sub OnAttached()
-			MyBase.OnAttached()
-			AddHandler AssociatedObject.CustomUnboundColumnData, AddressOf OnGridCustomUnboundColumnData
-			AddHandler AssociatedObject.Loaded, AddressOf OnGridLoaded
-		End Sub
-		Protected Overrides Sub OnDetaching()
-			RemoveHandler AssociatedObject.CustomUnboundColumnData, AddressOf OnGridCustomUnboundColumnData
-			RemoveHandler View.CellValueChanged, AddressOf OnViewCellValueChanged
-			MyBase.OnDetaching()
-		End Sub
-		Private Sub OnGridLoaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			RemoveHandler AssociatedObject.Loaded, AddressOf OnGridLoaded
-			AddHandler View.CellValueChanged, AddressOf OnViewCellValueChanged
-			CreateColumnsAndConditions()
-		End Sub
-		Private Sub OnViewCellValueChanged(ByVal sender As Object, ByVal e As CellValueChangedEventArgs)
-			If Not IsServiceColumn(e.Column) Then
-				Dim fieldName = $"{UnboundColumnPrefix}{e.Column.FieldName}"
-				Dim key = Tuple.Create(e.Row, fieldName)
-				Dim originalValue As Object = Nothing
-				Dim isModified As Boolean = True
-				If originalValues.TryGetValue(key, originalValue) Then
-					isModified = Not Equals(originalValue, e.Value)
-				Else
-					originalValues(key) = e.OldValue
-				End If
-				AssociatedObject.SetCellValue(e.RowHandle, fieldName, isModified)
-			End If
-		End Sub
-		Private Sub OnGridCustomUnboundColumnData(ByVal sender As Object, ByVal e As GridColumnDataEventArgs)
-			If IsServiceColumn(e.Column) Then
-				Dim key = Tuple.Create(AssociatedObject.GetRowByListIndex(e.ListSourceRowIndex), e.Column.FieldName)
-				If e.IsGetData Then
-					Dim res As Boolean = Nothing
-					e.Value = If(modifiedCells.TryGetValue(key, res), res, False)
-				End If
-				If e.IsSetData Then
-					modifiedCells(key) = CBool(e.Value)
-				End If
-			End If
-		End Sub
-		Private Shared Function IsServiceColumn(ByVal column As GridColumn) As Boolean
-			Return column.FieldName.StartsWith(UnboundColumnPrefix)
-		End Function
-		Private Sub CreateColumnsAndConditions()
-			For Each column As GridColumn In AssociatedObject.Columns.ToList()
-				Dim unboundColumn As New GridColumn()
-				unboundColumn.FieldName = UnboundColumnPrefix & column.FieldName
-				unboundColumn.UnboundType = UnboundColumnType.Boolean
-				unboundColumn.Visible = False
-				unboundColumn.ShowInColumnChooser = False
-				AssociatedObject.Columns.Add(unboundColumn)
-				View.FormatConditions.Add(New FormatCondition() With {
-					.FieldName = column.FieldName,
-					.Expression = String.Format("[{0}] = true", unboundColumn.FieldName),
-					.Format = New DevExpress.Xpf.Core.ConditionalFormatting.Format() With {.Background = HighlightBrush}
-				})
-			Next column
-		End Sub
-	End Class
+    Public Class ChangedCellsHighlightBehavior
+        Inherits DevExpress.Mvvm.UI.Interactivity.Behavior(Of DevExpress.Xpf.Grid.GridControl)
+
+        Const UnboundColumnPrefix As String = "IsEdited_"
+
+        Private modifiedCells As System.Collections.Generic.Dictionary(Of System.Tuple(Of Object, String), Boolean) = New System.Collections.Generic.Dictionary(Of System.Tuple(Of Object, String), Boolean)()
+
+        Private originalValues As System.Collections.Generic.Dictionary(Of System.Tuple(Of Object, String), Object) = New System.Collections.Generic.Dictionary(Of System.Tuple(Of Object, String), Object)()
+
+        Public Shared ReadOnly HighlightBrushProperty As System.Windows.DependencyProperty = System.Windows.DependencyProperty.Register("HighlightBrush", GetType(System.Windows.Media.Brush), GetType(HighlightModifiedCells.ChangedCellsHighlightBehavior))
+
+        Public Property HighlightBrush As Brush
+            Get
+                Return CType(Me.GetValue(HighlightModifiedCells.ChangedCellsHighlightBehavior.HighlightBrushProperty), System.Windows.Media.Brush)
+            End Get
+
+            Set(ByVal value As Brush)
+                Me.SetValue(HighlightModifiedCells.ChangedCellsHighlightBehavior.HighlightBrushProperty, value)
+            End Set
+        End Property
+
+        Protected ReadOnly Property View As TableView
+            Get
+                Return CType(Me.AssociatedObject.View, DevExpress.Xpf.Grid.TableView)
+            End Get
+        End Property
+
+        Protected Overrides Sub OnAttached()
+            MyBase.OnAttached()
+            AddHandler Me.AssociatedObject.CustomUnboundColumnData, AddressOf Me.OnGridCustomUnboundColumnData
+            AddHandler Me.AssociatedObject.Loaded, AddressOf Me.OnGridLoaded
+        End Sub
+
+        Protected Overrides Sub OnDetaching()
+            RemoveHandler Me.AssociatedObject.CustomUnboundColumnData, AddressOf Me.OnGridCustomUnboundColumnData
+            RemoveHandler Me.View.CellValueChanged, AddressOf Me.OnViewCellValueChanged
+            MyBase.OnDetaching()
+        End Sub
+
+        Private Sub OnGridLoaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            RemoveHandler Me.AssociatedObject.Loaded, AddressOf Me.OnGridLoaded
+            AddHandler Me.View.CellValueChanged, AddressOf Me.OnViewCellValueChanged
+            Me.CreateColumnsAndConditions()
+        End Sub
+
+        Private Sub OnViewCellValueChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CellValueChangedEventArgs)
+            If Not HighlightModifiedCells.ChangedCellsHighlightBehavior.IsServiceColumn(e.Column) Then
+                Dim fieldName = $"{HighlightModifiedCells.ChangedCellsHighlightBehavior.UnboundColumnPrefix} {e.Column.FieldName}"
+                Dim key = System.Tuple.Create(e.Row, fieldName)
+                Dim originalValue As Object
+                Dim isModified As Boolean = True
+                If Me.originalValues.TryGetValue(key, originalValue) Then
+                    isModified = Not System.[Object].Equals(originalValue, e.Value)
+                Else
+                    Me.originalValues(key) = e.OldValue
+                End If
+
+                Me.AssociatedObject.SetCellValue(e.RowHandle, fieldName, isModified)
+            End If
+        End Sub
+
+        Private Sub OnGridCustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.GridColumnDataEventArgs)
+            If HighlightModifiedCells.ChangedCellsHighlightBehavior.IsServiceColumn(e.Column) Then
+                Dim key = System.Tuple.Create(Me.AssociatedObject.GetRowByListIndex(e.ListSourceRowIndex), e.Column.FieldName)
+                If e.IsGetData Then
+                    Dim res As Boolean
+                    e.Value = If(Me.modifiedCells.TryGetValue(key, res), res, False)
+                End If
+
+                If e.IsSetData Then Me.modifiedCells(key) = CBool(e.Value)
+            End If
+        End Sub
+
+        Private Shared Function IsServiceColumn(ByVal column As DevExpress.Xpf.Grid.GridColumn) As Boolean
+            Return column.FieldName.StartsWith(HighlightModifiedCells.ChangedCellsHighlightBehavior.UnboundColumnPrefix)
+        End Function
+
+        Private Sub CreateColumnsAndConditions()
+            For Each column As DevExpress.Xpf.Grid.GridColumn In Me.AssociatedObject.Columns.ToList()
+                Dim unboundColumn As DevExpress.Xpf.Grid.GridColumn = New DevExpress.Xpf.Grid.GridColumn()
+                unboundColumn.FieldName = HighlightModifiedCells.ChangedCellsHighlightBehavior.UnboundColumnPrefix & column.FieldName
+                unboundColumn.UnboundDataType = GetType(Boolean)
+                unboundColumn.Visible = False
+                unboundColumn.ShowInColumnChooser = False
+                Me.AssociatedObject.Columns.Add(unboundColumn)
+                Me.View.FormatConditions.Add(New DevExpress.Xpf.Grid.FormatCondition() With {.FieldName = column.FieldName, .Expression = String.Format("[{0}] = true", unboundColumn.FieldName), .Format = New DevExpress.Xpf.Core.ConditionalFormatting.Format() With {.Background = Me.HighlightBrush}})
+            Next
+        End Sub
+    End Class
 End Namespace
--- VB/ViewModel.vb
+++ VB/ViewModel.vb
@@ -1,27 +1,25 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
 
 Namespace HighlightModifiedCells
-	Public Class ViewModel
-		Public Property Customers() As ObservableCollection(Of Customer)
-		Public Sub New()
-			Customers = New ObservableCollection(Of Customer)()
-			For i As Integer = 1 To 29
-				Customers.Add(New Customer() With {
-					.ID = i,
-					.Name = "Customer" & i,
-					.RegistrationDate = DateTime.Now.AddDays(i)
-				})
-			Next i
-		End Sub
-	End Class
 
-	Public Class Customer
-		Public Property ID() As Integer
-		Public Property Name() As String
-		Public Property RegistrationDate() As DateTime
-	End Class
+    Public Class ViewModel
+
+        Public Property Customers As ObservableCollection(Of Customer)
+
+        Public Sub New()
+            Customers = New ObservableCollection(Of Customer)()
+            For i As Integer = 1 To 30 - 1
+                Customers.Add(New Customer() With {.ID = i, .Name = "Customer" & i, .RegistrationDate = Date.Now.AddDays(i)})
+            Next
+        End Sub
+    End Class
+
+    Public Class Customer
+
+        Public Property ID As Integer
+
+        Public Property Name As String
+
+        Public Property RegistrationDate As Date
+    End Class
 End Namespace

a70780284856d6c9d1ce8dbeeea980274c56652a
DevExpressExampleBot <fakeEmail>
2023-02-21 19:44:03 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,27 +1,13 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports DevExpress.Xpf.Grid
-Imports DevExpress.Data
-Imports DevExpress.Xpf.Grid.Themes
 Imports DevExpress.Xpf.Core
 
 Namespace HighlightModifiedCells
-	Partial Public Class Window1
-		Inherits DXWindow
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Window1
+        Inherits DXWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
